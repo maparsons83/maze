41c6dc28d4f47c8470f2c31f683be2fd8f35a83f
Jake Berg <jacobvictorberg@gmail.com>
2018-03-05 14:21:22 -0500

    down doesn't work

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// var boxTop = 200;
-// var boxleft = 200;
-currentPos = '90';
-
-var map = [
+const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
     "W W W WWW WWWWW W W W",
@@ -20,24 +16,25 @@ var map = [
     "WWWWWWWWWWWWWWWWWWWWW"
 ];
 
-// create array of blocks 
-function draw() {
-    var main = document.querySelector("main");
+const main = document.querySelector("main");
 
 
-    for (let i = 0; i < map.length; i++) {
+
+function draw(startPos) {
+
+
+    for (let i = 0; i < startPos.length; i++) {
         var newRow = document.createElement("div");
         newRow.className = ("flexbox");
         main.appendChild(newRow);
 
-        for (let j = 0; j < map[i].length; j++) {
-            let cell = map[i][j];
+        for (let j = 0; j < startPos[i].length; j++) {
+            let cell = startPos[i][j];
 
             if (cell === "W") {
                 var newCol = document.createElement("div");
                 newCol.className = ("wall");
                 newRow.appendChild(newCol);
-
             } else if (cell === " ") {
                 var newCol = document.createElement("div");
                 newCol.className = ("empty");
@@ -47,7 +44,6 @@ function draw() {
                 newCol.id = ("start");
                 let start = document.getElementById("start");
                 newRow.appendChild(newCol);
-
             } else if (cell === "F") {
                 var newCol = document.createElement("div");
                 newCol.className = ("finish");
@@ -55,41 +51,118 @@ function draw() {
             }
         }
     }
-    // console.log(main);
-
 }
 
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
+    reset(main);
 
     if (keyName == "ArrowRight") {
 
         for (let i = 0; i < map.length; i++) {
 
             if (map[i].includes("S")) {
-                playerPos = map[i].indexOf("S");
-                
-                
+                let mapSplit = map[i].split('');
+                let playerPos = map[i].indexOf("S");
+                if (map[i][playerPos + 1] == " ") {
+                    mapSplit.splice(playerPos, 1);
+                    mapSplit.splice((playerPos + 1), 0, "S");
+                    let rejoined = mapSplit.join('');
+                    map[i] = rejoined;
+                    var newMap = map;
+                    console.log(newMap)
+                } else if(map[i][playerPos+1] == "F"){
+                    alert("you win");
+                } else {
+                    var newMap = map;
+                }
             }
-
-
-
         }
     }
+    if (keyName == "ArrowLeft") {
+
+        for (let i = 0; i < map.length; i++) {
+
+            if (map[i].includes("S")) {
+                let mapSplit = map[i].split('');
+                let playerPos = map[i].indexOf("S");
+                if (map[i][playerPos - 1] == " ") {
+                    mapSplit.splice(playerPos, 1);
+                    mapSplit.splice((playerPos - 1), 0, "S");
+                    let rejoined = mapSplit.join('');
+                    map[i] = rejoined;
+                    var newMap = map;
+                } else {
+                    var newMap = map;
+                }
+            }
+        }
+
+    }
+    if (keyName == "ArrowUp") {
+
+        for (let i = 0; i < map.length; i++) {
+
+            if (map[i].includes("S")) {
+                let mapSplit = map[i].split('');
+                let rowAbove = map[i-1].split('');
+                let playerPos = map[i].indexOf("S");
+
+                if (map[i-1][playerPos] == " ") {
+                    mapSplit.splice(playerPos, 1, " ");
+                    rowAbove.splice(playerPos, 1, "S");
+
+                    let rejoined = mapSplit.join('');
+                    map[i] = rejoined;
+
+                    let rejoined2 = rowAbove.join('');
+                    map[i-1] = rejoined2;
+
+                    var newMap = map;
+                } else {
+                    var newMap = map;
+                }
+            }
+        }
+
+    }
+    if (keyName == "ArrowDown") {
+
+        for (let i = 0; i < map.length; i++) {
+
+            if (map[i].includes("S")) {
+                let mapSplit = map[i].split('');
+                let rowBelow = map[i+1].split('');
+                let playerPos = map[i].indexOf("S");
+
+                if (map[i+1][playerPos] == " ") {
+                    mapSplit.splice(playerPos, 1, " ");
+                    rowBelow.splice(playerPos, 1, "S");
+
+                    let rejoined = mapSplit.join('');
+                    map[i] = rejoined;
+
+                    let rejoined2 = rowBelow.join('');
+                    map[i+1] = rejoined2;
+
+                    var newMap = map;
+                } else {
+                    var newMap = map;
+                }
+            }
+        }
+
+    }
+    
+    draw(newMap);
 
 });
 
-draw();
+function reset(destination) {
+    while (destination.firstChild) {
+        destination.removeChild(destination.firstChild);
+    }
+}
 
-
-
-
-
-// for (let j = 0; j < map[i].length; j++) {
-//     let cell = map[i][j];
-
-//     if (cell === "S") {
-//         cell = cell[i][j + 1];
-
-
-//     }
+draw(map);
+console.log(main)
